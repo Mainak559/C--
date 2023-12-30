67f816eeb971bfea87ffff312e44d75d05e57ff9
Mainak Halder <mainakhalder5598633@gmail.com>
2023-12-30 17:35:58 +0530

    C++ Template for CP

---
+++ demo.cpp
@@ -0,0 +1,18 @@
+#include<bits/stdc++.h>
+typedef unsigned long long int l1;
+
+using namespace std;
+int main()
+{
+
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    #ifndef ONLINE_JUDGE
+    freopen("input.txt","r", stdin);
+    freopen("output.txt","w",stdout);
+    #endif
+    
+    cout<<"Hello World : " ;
+    return 0;
+}
